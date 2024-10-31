d659e480a973928cafab46b4848623fdc98c2251
mpardo24 <113159056+mpardo24@users.noreply.github.com>
2024-10-31 20:20:06 -0300

    Modificacion de MainMenuScreen
    Se realiza la modificacion de la clase main menu, donde se reemplaza el mensaje principal por una imagen, la cual contiene la bienvenida y los controles a utilizar dentro del juego.

--- GameLluviaMenu2024/core/src/main/java/puppy/code/MainMenuScreen.java
+++ GameLluviaMenu2024/core/src/main/java/puppy/code/MainMenuScreen.java
@@ -3,80 +3,69 @@ package puppy.code;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-
 public class MainMenuScreen implements Screen {
 
-	final GameLluviaMenu game;
-	private SpriteBatch batch;
-	private BitmapFont font;
-	private OrthographicCamera camera;
+    final GameLluviaMenu game;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Texture menuImagen;  // Nueva variable para cargar la imagen del menú
 
-	public MainMenuScreen(final GameLluviaMenu game) {
-		this.game = game;
+    public MainMenuScreen(final GameLluviaMenu game) {
+        this.game = game;
         this.batch = game.getBatch();
-        this.font = game.getFont();
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-	}
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
 
-	@Override
-	public void render(float delta) {
-		ScreenUtils.clear(0, 0, 0.2f, 1);
+        // Cargar la imagen del menú desde la carpeta de assets
+        menuImagen = new Texture(Gdx.files.internal("menu_de_inicio.png"));  // Asegúrate de que la imagen esté en la carpeta "assets"
+    }
 
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
+    @Override
+    public void render(float delta) {
+        // Limpiar la pantalla con un color oscuro
+        ScreenUtils.clear(0, 0, 0.2f, 1);
 
-		batch.begin();
-		font.getData().setScale(2, 2);
-		font.draw(batch, "Bienvenido a Recolecta Gotas!!! ", 100, camera.viewportHeight/2+50);
-		font.draw(batch, "Toca en cualquier lugar para comenzar!", 100, camera.viewportHeight/2-50);
+        // Actualizar la cámara
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
 
-		batch.end();
+        // Iniciar el batch para dibujar la imagen del menú
+        batch.begin();
 
-		if (Gdx.input.isTouched()) {
-			game.setScreen(new GameScreen(game));
-			dispose();
-		}
-	}
+        // Dibujar la imagen del menú en toda la pantalla
+        batch.draw(menuImagen, 0, 0, 800, 480);
 
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
-	}
+        batch.end();
 
-	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
+        // Detectar si se toca la pantalla para empezar el juego
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));  // Cambiar a la pantalla del juego principal
+            dispose();
+        }
+    }
 
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void show() {}
 
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void resize(int width, int height) {}
 
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void pause() {}
 
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void resume() {}
 
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        // Liberar la imagen cuando ya no se necesite
+        menuImagen.dispose();
+    }
 }
