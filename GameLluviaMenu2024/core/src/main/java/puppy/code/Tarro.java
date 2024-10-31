29d23669921a7ef70a7f4a470d7f7bcddaf86b34
mpardo24 <113159056+mpardo24@users.noreply.github.com>
2024-10-31 20:24:25 -0300

    Modificacion de Tarro
    Se realiza la modificacion de la clase tarro, la cual maneja al personaje principal. Añadiendo a este la posibilidad de que al llegar al margen de la ventana sea teletransportado al otro, siguiendo una lógica similar a la de "Pac-Man"

--- GameLluviaMenu2024/core/src/main/java/puppy/code/Tarro.java
+++ GameLluviaMenu2024/core/src/main/java/puppy/code/Tarro.java
@@ -8,87 +8,87 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 
-
 public class Tarro {
-	   private Rectangle bucket;
-	   private Texture bucketImage;
-	   private Sound sonidoHerido;
-	   private int vidas = 3;
-	   private int puntos = 0;
-	   private int velx = 400;
-	   private boolean herido = false;
-	   private int tiempoHeridoMax=50;
-	   private int tiempoHerido;
-	   
-	   
-	   public Tarro(Texture tex, Sound ss) {
-		   bucketImage = tex;
-		   sonidoHerido = ss;
-	   }
-	   
-		public int getVidas() {
-			return vidas;
-		}
-	
-		public int getPuntos() {
-			return puntos;
-		}
-		public Rectangle getArea() {
-			return bucket;
-		}
-		public void sumarPuntos(int pp) {
-			puntos+=pp;
-		}
-		
-	
-	   public void crear() {
-		      bucket = new Rectangle();
-		      bucket.x = 800 / 2 - 64 / 2;
-		      bucket.y = 20;
-		      bucket.width = 64;
-		      bucket.height = 64;
-	   }
-	   public void dañar() {
-		  vidas--;
-		  herido = true;
-		  tiempoHerido=tiempoHeridoMax;
-		  sonidoHerido.play();
-	   }
-	   public void dibujar(SpriteBatch batch) {
-		 if (!herido)  
-		   batch.draw(bucketImage, bucket.x, bucket.y);
-		 else {
-		
-		   batch.draw(bucketImage, bucket.x, bucket.y+ MathUtils.random(-5,5));
-		   tiempoHerido--;
-		   if (tiempoHerido<=0) herido = false;
-		 }
-	   } 
-	   
-	   
-	   public void actualizarMovimiento() { 
-		   // movimiento desde mouse/touch
-		   /*if(Gdx.input.isTouched()) {
-			      Vector3 touchPos = new Vector3();
-			      touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			      camera.unproject(touchPos);
-			      bucket.x = touchPos.x - 64 / 2;
-			}*/
-		   //movimiento desde teclado
-		   if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += velx * Gdx.graphics.getDeltaTime();
-		   // que no se salga de los bordes izq y der
-		   if(bucket.x < 0) bucket.x = 0;
-		   if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-	   }
-	    
+    private Rectangle bucket;
+    private Texture bucketImage;
+    private Sound sonidoHerido;
+    private int vidas = 3;
+    private int puntos = 0;
+    private int velx = 400;
+    private boolean herido = false;
+    private int tiempoHeridoMax = 50;
+    private int tiempoHerido;
 
-	public void destruir() {
-		    bucketImage.dispose();
-	   }
-	
-   public boolean estaHerido() {
-	   return herido;
-   }
-	   
+    public Tarro(Texture tex, Sound ss) {
+        bucketImage = tex;
+        sonidoHerido = ss;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public Rectangle getArea() {
+        return bucket;
+    }
+
+    public void sumarPuntos(int pp) {
+        puntos += pp;
+    }
+
+    public void crear() {
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2;
+        bucket.y = 20;
+        bucket.width = 64;
+        bucket.height = 64;
+    }
+
+    public void dañar() {
+        vidas--;
+        herido = true;
+        tiempoHerido = tiempoHeridoMax;
+        sonidoHerido.play();
+    }
+
+    public void dibujar(SpriteBatch batch) {
+        if (!herido)
+            batch.draw(bucketImage, bucket.x, bucket.y);
+        else {
+            batch.draw(bucketImage, bucket.x, bucket.y + MathUtils.random(-5, 5));
+            tiempoHerido--;
+            if (tiempoHerido <= 0) herido = false;
+        }
+    }
+
+    public void actualizarMovimiento() {
+        // movimiento desde teclado
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= velx * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += velx * Gdx.graphics.getDeltaTime();
+
+        // Lógica para teletransporte
+        if (bucket.x < 0) {
+            bucket.x = 800 - 64; // Teletransportar al lado derecho
+        }
+        if (bucket.x > 800 - 64) {
+            bucket.x = 0; // Teletransportar al lado izquierdo
+        }
+    }
+
+    public void destruir() {
+        bucketImage.dispose();
+    }
+
+    public boolean estaHerido() {
+        return herido;
+    }
+    public void sumarVida(int cantidad) {
+        vidas += cantidad;
+        if (vidas > 3) vidas = 3;  // Limitar a 3 vidas máximo
+    }
+
 }
