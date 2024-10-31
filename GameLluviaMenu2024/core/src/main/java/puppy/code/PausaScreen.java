dfbb25e0805d7e0818e9d507899695ab616ccb35
mpardo24 <113159056+mpardo24@users.noreply.github.com>
2024-10-31 20:21:51 -0300

    Modificacion de PausaScreen
    Se realiza la modificacion de la pantalla de pausa la cual es reemplazada por una imagen acorde a nuestro juego

--- GameLluviaMenu2024/core/src/main/java/puppy/code/PausaScreen.java
+++ GameLluviaMenu2024/core/src/main/java/puppy/code/PausaScreen.java
@@ -1,83 +1,76 @@
 package puppy.code;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-
 public class PausaScreen implements Screen {
 
-	private final GameLluviaMenu game;
-	private GameScreen juego;
-	private SpriteBatch batch;	   
-	private BitmapFont font;
-	private OrthographicCamera camera;
+    private final GameLluviaMenu game;
+    private final GameScreen juego;
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private OrthographicCamera camera;
+    private Texture pausaImagen;  // Imagen para la pausa
 
-	public PausaScreen (final GameLluviaMenu game, GameScreen juego) {
-		this.game = game;
+    public PausaScreen(final GameLluviaMenu game, GameScreen juego) {
+        this.game = game;
         this.juego = juego;
         this.batch = game.getBatch();
         this.font = game.getFont();
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-	}
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
 
-	@Override
-	public void render(float delta) {
-		ScreenUtils.clear(0, 0, 1.0f, 0.5f);
+        // Cargar la imagen de pausa desde los assets
+        pausaImagen = new Texture(Gdx.files.internal("pausa.png"));  // Imagen guardada como pausa.png en assets
+    }
 
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 1.0f, 0.5f);
 
-		batch.begin();
-		font.draw(batch, "Juego en Pausa ", 100, 150);
-		font.draw(batch, "Toca en cualquier lado para continuar !!!", 100, 100);
-		batch.end();
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
 
-		if (Gdx.input.isTouched()) {
-			game.setScreen(juego);
-			dispose();
-		}
-	}
+        batch.begin();
 
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
-	}
+        // Dibujar la imagen de pausa
+        batch.draw(pausaImagen, 0, 0, 800, 480);  // Dibuja la imagen en toda la pantalla
 
-	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
+        font.draw(batch, "", 100, 150);
+        font.draw(batch, "", 100, 100);
+        batch.end();
 
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
+        // Verificar si se presiona "ESC" para reanudar el juego
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            game.setScreen(juego);  // Regresa al juego
+            dispose();
+        }
+    }
 
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void show() {}
 
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void resize(int width, int height) {}
 
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void pause() {}
 
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        // Liberar la imagen de pausa cuando ya no se necesite
+        pausaImagen.dispose();
+    }
 }
-
