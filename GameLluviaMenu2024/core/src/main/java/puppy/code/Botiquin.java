ec52b19415847bc71a70c9d377d729d8c9676794
mpardo24 <113159056+mpardo24@users.noreply.github.com>
2024-11-24 23:03:49 -0300

    Actualización de la clase base Botiquin
    Se mejoró la encapsulación de atributos como posición y textura.
    Se agregó soporte para estrategias de movimiento utilizando la interfaz EstrategiaMovimiento.
    La clase ahora es más extensible para admitir futuras modificaciones y variantes.

--- GameLluviaMenu2024/core/src/main/java/puppy/code/Botiquin.java
+++ GameLluviaMenu2024/core/src/main/java/puppy/code/Botiquin.java
@@ -6,47 +6,23 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
-public abstract class Botiquin {
-    protected Rectangle area;
-    protected Texture texture;
-    protected float velocidadCaida = 100;  // Velocidad de caída en píxeles por segundo
+public abstract class Botiquin extends ObjetoCayendo {
     protected Sound ladridoSound;  // Nuevo atributo para el sonido del ladrido
 
-    public Botiquin(Texture texture, float x, float y, float width, float height, Sound ladridoSound) {
-        this.texture = texture;
-        this.area = new Rectangle(x, y, width, height);
+    public Botiquin(Texture textura, float x, float y, float ancho, float alto, EstrategiaMovimiento estrategia, Sound ladridoSound) {
+        super(textura, x, y, ancho, alto, estrategia);  // Llama al constructor de ObjetoCayendo
         this.ladridoSound = ladridoSound;  // Guardar el sonido
     }
 
     // Método abstracto para aplicar el efecto al tarro
     public abstract void aplicarEfecto(Tarro tarro);
 
-    // Método para actualizar la posición de caída del botiquín
-    public void actualizarMovimiento(Tarro tarro) {
-        if (!tarro.estaHerido()) {  // Solo se moverá si el tarro no está herido
-            area.y -= velocidadCaida * Gdx.graphics.getDeltaTime();
-        }
-    }
-
-    public void dibujar(SpriteBatch batch) {
-        batch.draw(texture, area.x, area.y, area.width, area.height);
-    }
-
-    public Rectangle getArea() {
-        return area;
-    }
-
-    public boolean fueraDePantalla() {
-        return area.y + area.height < 0;  // Retorna true si el botiquín salió de la pantalla
-    }
-
     public void destruir() {
-        texture.dispose();
+        textura.dispose();
     }
 
     // Reproducir el sonido del ladrido
     protected void reproducirLadrido() {
-        // Reproducir sonido solo si está activado
         if (ConfiguracionJuegoSingleton.obtenerInstancia().audioEstaActivado() && ladridoSound != null) {
             ladridoSound.play();
         }
